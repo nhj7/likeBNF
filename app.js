dfafb6e308979299bf7841a29c0bd19093608d9e
nhj12311 <nhj12311@gmail.com>
2021-01-13 22:27:41 +0900

    c

--- app.js
+++ app.js
@@ -88,6 +88,8 @@ const server = app.listen(process.env.PORT || 3000, () => {
 });
 
 /* Prevent Sleep in Heroku Server */
+var http = require("http");
+//http.get(process.env.PORT ? "https://like-bnf.herokuapp.com":"http://localhost:3000");
 setInterval(function () {
     console.log("setInterval.");
     http.get(process.env.PORT ? "https://like-bnf.herokuapp.com":"http://localhost:3000");
